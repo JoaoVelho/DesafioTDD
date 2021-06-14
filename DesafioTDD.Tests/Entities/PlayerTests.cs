4aa8ce7c042189616234a6e1c85e030894986b0d
jrrl <joao.velho@gft.com>
2021-06-14 14:24:11 -0300

    make player tests

---
+++ DesafioTDD.Tests/Entities/PlayerTests.cs
@@ -0,0 +1,27 @@
+using System;
+using DesafioTDD.Domain.Entities;
+using DesafioTDD.Domain.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesafioTDD.Tests.Entities
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void ShouldReturnFalseIfFailToValidate() {
+            Enum.TryParse("4", out EHands play1);
+            Enum.TryParse("0", out EHands play2);
+
+            Assert.AreEqual(Player.Validate(play1), false);
+            Assert.AreEqual(Player.Validate(play2), false);
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueIfValidate() {
+            Enum.TryParse("2", out EHands play);
+
+            Assert.AreEqual(Player.Validate(play), true);
+        }
+    }
+}

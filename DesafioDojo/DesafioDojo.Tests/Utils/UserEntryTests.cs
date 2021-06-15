5734f7107397ea9a05c1471acb9fbf6533da1156
jrrl <joao.velho@gft.com>
2021-06-15 15:42:52 -0300

    make userentry tests

---
+++ DesafioDojo/DesafioDojo.Tests/Utils/UserEntryTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using DesafioDojo.Domain.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesafioDojo.Tests.Utils
+{
+    [TestClass]
+    public class UserEntryTests
+    {
+        [TestMethod]
+        public void ShouldReturnZeroIfNonNumericIsPassed() {
+            var input = new StringReader("abc");
+            Console.SetIn(input);
+
+            int number = UserEntry.GetNumber();
+            Assert.AreEqual(number, 0);
+        }
+
+        [TestMethod]
+        public void ShouldReturnZeroIfNumberLessThanOneIsPassed() {
+            var input = new StringReader("-5");
+            Console.SetIn(input);
+
+            int number = UserEntry.GetNumber();
+            Assert.AreEqual(number, 0);
+        }
+
+        [TestMethod]
+        public void ShouldReturnTheNumberPassedIfItIsValid() {
+            var input = new StringReader("4");
+            Console.SetIn(input);
+
+            int number = UserEntry.GetNumber();
+            Assert.AreEqual(number, 4);
+        }
+    }
+}
